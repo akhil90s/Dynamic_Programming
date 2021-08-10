7e338aa3a8b7c27aae3274ce8a7982e0124c4157
akhil90s <akhil.chawla2010@gmail.com>
2021-08-10 22:07:08 +0530

    DYNAMIC_PROGRAMMING

--- DYNAMIC_PROGRAMMING/src/ARRAY/Unique_Path_II.java
+++ DYNAMIC_PROGRAMMING/src/ARRAY/Unique_Path_II.java
@@ -4,8 +4,7 @@ public class Unique_Path_II {
 
     public static void main(String[] args) {
 
-        // int[][] array = new int[][] { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 } };
-        int[][] array = new int[][] { { 0, 1 }, { 0, 0 } };
+        int[][] array = new int[][] { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 } };
 
         int result_uniquePathsWithObstacles = uniquePathsWithObstacles(array);
         System.out.println(result_uniquePathsWithObstacles);
@@ -14,18 +13,36 @@ public class Unique_Path_II {
 
     public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
 
-        if (obstacleGrid.length == 0 || obstacleGrid == null)
+        if (obstacleGrid.length == 0 || obstacleGrid == null || obstacleGrid[0][0] == 1) {
             return 0;
+        }
 
         int row = obstacleGrid.length;
         int column = obstacleGrid[0].length;
 
         int[][] dp = new int[row][column];
+        
+        // First Row
+        for (int i = 0; i < column; i++) {
+            if (obstacleGrid[0][i] == 0) {
+                dp[0][i] = 1;
+            } else {
+                break;
+            }
+        }
+
+        // First Column
         for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                if ((i == 0 || j == 0) && obstacleGrid[i][j] != 1) {
-                    dp[i][j] = 1;
-                } else if (obstacleGrid[i][j] == 1) {
+            if (obstacleGrid[i][0] == 0) {
+                dp[i][0] = 1;
+            } else {
+                break;
+            }
+        }
+
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < column; j++) {
+                if (obstacleGrid[i][j] == 1) {
                     dp[i][j] = 0;
                 } else {
                     dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
